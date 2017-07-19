7c1f523f30d596f5478b2f26afd0a536a2a71f24
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-07-18 17:28:51 -0700

    Made it work!

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,16 +1,19 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
+let express = require('express');
+let app = express();
+
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
-
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('/public'));
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
 
@@ -23,5 +26,6 @@ app.post('/articles', bodyParser, function(request, response) {
 })
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`'listening on port number: ${PORT}'`);
+  // DONE: Log to the console a message that lets you know which port your server has started on
 });
